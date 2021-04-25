fb51e229ab68ed85cdd7c827481f1f40a395782c
anitkumarsingh <anikumarsingh99@gmail.com>
2021-04-26 04:11:05 +0530

    minor change

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 import hackersRouter from './routes/hackers.js';
 import usersRouter from './routes/users.js';
 import dotenv from 'dotenv';
@@ -16,6 +17,21 @@ const PORT = process.env.PORT || 5000;
 
 app.use('/api/hackers', hackersRouter);
 app.use('/api/users', usersRouter);
+const __dirname = path.resolve();
+
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+	app.get('*', (req, res) =>
+		res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+	);
+} else {
+	app.get('/', (req, res) => {
+		res.send('Hello API is running....');
+	});
+}
 
 app.use(notFound);
 
